c02a0e1004d40205375bef7ef00741c1628cd0ff
Marcos <contato.shopvmax@gmail.com>
2022-07-27 17:54:33 -0300

    initialization with typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
 import express from "express";
+import "reflect-metadata";
 
 const app = express();
+
+app.get("/", (req, res) => {
+  return res.send("Olá NLW O5");
+});
+app.post("/", (req, res) => {
+  return res.json({ message: "Olá NLW O5" });
+});
 
 app.listen(3331, () => console.log("servidor rodando!"));
